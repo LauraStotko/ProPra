cf9ff372e0d7a468cb811de7b3b2e490eae568dd
Diana Bilic <bilic@cip.ifi.lmu.de>
2024-02-27 13:24:11 +0100

    Adds a function for extraction of an accession number

--- Solution1/Assignement6/spkeyword.py
+++ Solution1/Assignement6/spkeyword.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 
 def parse_arguments():
@@ -9,7 +10,41 @@ def parse_arguments():
     return parser.parse_args()
 
 
-# Test the argument parsing
-args = parse_arguments()
-print(f'Keyword: {args.keyword}')
-print(f'Swissprot File: {args.swissprot.name}')
+def parse_swissprot_accession_number(ac_nr):
+    """
+    Parses a Swissprot entry to extract AC (Accession) numbers.
+
+    Parameters:
+    - ac_nr (str): The portion of the Swissprot entry containing the AC numbers.
+
+    Returns:
+    - list: A list of AC numbers extracted from the provided string.
+
+    The function uses a regular expression to search for AC numbers in the provided string.
+    Looks for the following pattern:
+    - `AC   `: Matches the literal string "AC   ".
+    - `(\w+)`: Captures group for multiple word characters -> represents AC number.
+    - `;`: Matches semicolon at the end of each AC number.
+    """
+    ac_numbers = re.findall(r'AC   (\w+);', ac_nr)
+    return ac_numbers
+
+
+def main():
+    args = parse_arguments()
+
+    # Test the argument parsing
+    print(f'Keyword: {args.keyword}')
+    print(f'Swissprot File: {args.swissprot.name}')
+
+    # Test extraction of AC numbers
+    entry = """
+    AC   A12345;
+    AC   C67890;
+    """
+    ac_numbers = parse_swissprot_accession_number(entry)
+    print(ac_numbers)
+
+
+if __name__ == "__main__":
+    main()
