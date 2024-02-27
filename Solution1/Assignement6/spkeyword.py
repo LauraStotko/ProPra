8f47d75b47278ce1c61719ad2dca413aa2f5c978
Diana Bilic <bilic@cip.ifi.lmu.de>
2024-02-27 21:48:07 +0100

    Working Version (Server 100%) Fixes issue with '//' appearing e.g. in links (messed with separation of entries) Adds shebang Removes duplicate AC numbers
    TO DO
    - CGI, clean up code duplicates (e.g. line 69 & 82)

--- Solution1/Assignement6/spkeyword.py
+++ Solution1/Assignement6/spkeyword.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
+
 import argparse
-import re
 
 
 def parse_arguments():
@@ -10,40 +11,88 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def parse_swissprot_accession_number(ac_nr):
-    """
-    Parses a Swissprot entry to extract AC (Accession) numbers.
+def parse_entry_keywords(line):
+    list_of_entries_keys = []
+    line = line.strip('KW   ')
+    line = line.replace('.', ';')
 
-    Parameters:
-    - ac_nr (str): The portion of the Swissprot entry containing the AC numbers.
+    for single_keyword in line.split(';'):
+        if len(single_keyword) != 0:
+            if single_keyword[0].isspace():
+                single_keyword = single_keyword[1:]
+            list_of_entries_keys.append(single_keyword)
 
-    Returns:
-    - list: A list of AC numbers extracted from the provided string.
+    return list_of_entries_keys
 
-    The function uses a regular expression to search for AC numbers in the provided string.
-    Looks for the following pattern:
-    - `AC   `: Matches the literal string "AC   ".
-    - `(\w+)`: Captures group for multiple word characters -> represents AC number.
-    - `;`: Matches semicolon at the end of each AC number.
-    """
-    ac_numbers = re.findall(r'AC   (\w+);', ac_nr)
-    return ac_numbers
+
+def parse_entry_ac(line):
+    ac_list = []
+    line = line.strip('AC   ')
+
+    for ac_number in line.split(';'):
+        if len(ac_number) != 0:
+            ac_number = ac_number.strip()
+            ac_list.append(ac_number)
+    return ac_list
+
+
+def print_ac_list(list_of_all_acs):
+    for ac_nr in list_of_all_acs:
+        print(ac_nr)
 
 
 def main():
     args = parse_arguments()
 
-    # Test the argument parsing
-    print(f'Keyword: {args.keyword}')
-    print(f'Swissprot File: {args.swissprot.name}')
+    # Get keywords & save as set to remove duplicates
+    keywords = set(args.keyword)
 
-    # Test extraction of AC numbers
-    entry = """
-    AC   A12345;
-    AC   C67890;
-    """
-    ac_numbers = parse_swissprot_accession_number(entry)
-    print(ac_numbers)
+    # List of all AC numbers
+    all_ac_numbers = []
+
+    list_of_entry_keywords = []
+    list_of_entry_ac = []
+    found_matching_keywords = False
+
+    # Get each entry from the Swissprot file
+    # Each entry is seperated by '//'
+    for line in args.swissprot.read().splitlines():
+
+        if line.startswith('KW'):
+            list_of_entry_keywords.extend(parse_entry_keywords(line))
+
+        if line.startswith('AC'):
+            list_of_entry_ac.extend(parse_entry_ac(line))
+
+        if line.startswith('//'):
+
+            for current_keyword in list_of_entry_keywords:
+
+                for input_keyword in keywords:
+                    if current_keyword == input_keyword:
+                        found_matching_keywords = True
+
+            if found_matching_keywords:
+                all_ac_numbers.extend(list_of_entry_ac)
+
+            list_of_entry_keywords = []
+            list_of_entry_ac = []
+            found_matching_keywords = False
+
+    for current_keyword in list_of_entry_keywords:
+
+        for input_keyword in keywords:
+            if current_keyword == input_keyword:
+                found_matching_keywords = True
+
+    if found_matching_keywords:
+        all_ac_numbers.extend(list_of_entry_ac)
+
+    cleaned_ac_numbers = list(set(all_ac_numbers))
+    cleaned_ac_numbers.sort()
+
+    for ac_nr in cleaned_ac_numbers:
+        print(ac_nr)
 
 
 if __name__ == "__main__":
