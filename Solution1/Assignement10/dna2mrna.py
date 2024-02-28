fa7ee307ce78042817d6aa7a8d8e8031ce0232de
Aurelia Charlotte Dersch <dersch@cip.ifi.lmu.de>
2024-02-28 14:12:51 +0100

    Replace dna2mrna.py with final version

--- Solution1/Assignement10/dna2mrna.py
+++ Solution1/Assignement10/dna2mrna.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 import argparse
-import sys
 
 def read_fasta(fasta_file):
 
     sequences = {}
     current_id = None
     current = ''
+    # go trough every line of the file and save the sequence_id with the corresponding sequence in a dictionary
     for l in fasta_file:
+        # returns the same string without spaces
         l = l.strip()
+        # first line of new sequence, contains the sequence_id
         if l.startswith('>'):
             if current_id:
                 sequences[current_id] = current
                 current=''
-            #neu anfangen
+            #save new id
             current_id = l.strip()[1:]
         else:
+            # add line of sequence to the current sequence
             current += l
     if current_id:
         sequences[current_id] = current
@@ -26,33 +29,27 @@ def dna2mrna(infile):
     mrna_sequences = {}
     for sequence_id, sequence in sequences.items():
         mrna = dna_to_mrna(sequence)
+        # save in new dictionary
         mrna_sequences[sequence_id] = mrna
     return mrna_sequences
 
 
 def dna_to_mrna(dna_sequence):
     mrna_sequence = ""
+    # replace T with U for mrna
     for base in dna_sequence:
-        if base == 'A':
+        if base == 'T':
             mrna_sequence += 'U'
-        elif base == 'T':
-            mrna_sequence += 'A'
-        elif base == 'C':
-            mrna_sequence += 'G'
-        elif base == 'G':
-            mrna_sequence += 'C'
+        else:
+            mrna_sequence += base
     return mrna_sequence
 
 if __name__ == '__main__':
     pars = argparse.ArgumentParser(description="Translate DNA to mRNA")
-    pars.add_argument("--fasta", type = str)
+    pars.add_argument("--fasta", type = argparse.FileType('r'), required=True)
     args = pars.parse_args()
 
-    if args.fasta:
-        with open(args.fasta, 'r') as fasta_file:
-            mrna_sequences = dna2mrna(fasta_file)
-    else:
-        mrna_sequences = dna2mrna(sys.stdin)
+    mrna_sequences = dna2mrna(args.fasta)
 
     for sequence_id, sequence in mrna_sequences.items():
         print(f'>{sequence_id}')
