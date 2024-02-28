be23f673e1d19fb3f711f31faaa81bb09b6424e2
Aurelia Charlotte Dersch <dersch@cip.ifi.lmu.de>
2024-02-28 18:32:56 +0100

    Replace genome_length.py by final version

--- Solution1/Assignement8/genome_length.py
+++ Solution1/Assignement8/genome_length.py
@@ -10,6 +10,7 @@ def download_genomeReport(url):
 def search_genome(report, organisms):
     genomes = []
     for l in report.decode().split('\n'):
+        # header
         if l.startswith('#'):
             continue
         columns = l.split('\t')
@@ -21,6 +22,7 @@ def search_genome(report, organisms):
         organism = columns[0]
         organism = organism.split("'")[0]
         for org in organisms:
+            # search for regex in the organism names
             if re.search(org, organism):
                 length = float(columns[6])
                 genomes.append((organism, length))
@@ -33,9 +35,13 @@ if __name__ == '__main__':
     pars.add_argument("--organism", help="Specify at least one organism to be searched for!", nargs='+')
     args = pars.parse_args()
 
+    # link for the genome report
     report_url = "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt"
     report = download_genomeReport(report_url)
+    #if download was successfull
     if report:
+        # Search the Genome report for the specified organisms
         search_results = search_genome(report, args.organism)
+        # print the name of the organism and the genome length in Mb
         for organism, length in search_results:
             print(f"{organism}\t{length}")
