26e30ec2674d9653dc3fc735a9bdad829c45118e
Diana Bilic <bilic@cip.ifi.lmu.de>
2024-02-28 18:24:59 +0100

    Update psscan.py to work for single sequence

--- Solution1/Assignement7/psscan.py
+++ Solution1/Assignement7/psscan.py
@@ -3,6 +3,7 @@
 import argparse
 import re
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Find the PROSITE pattern in FASTA sequences.")
     parser.add_argument("--fasta", type=argparse.FileType("r"), help="Input FASTA file or '-' for stdin", required=True)
@@ -12,37 +13,85 @@ def parse_arguments():
 
 def prosite_to_regex(prosite_pattern):
     regex_pattern = prosite_pattern
-    regex_pattern = regex_pattern.replace('-', '')      # Separation between different elements
+    regex_pattern = regex_pattern.replace('-', '')  # Separation between different elements
     regex_pattern = regex_pattern.replace(' - ', '')
-    regex_pattern = regex_pattern.replace('x', '.')     # Any character except newline
-    regex_pattern = regex_pattern.replace('{', '[^')    # [^abc] ->	not a, b, or c
+    regex_pattern = regex_pattern.replace('x', '.')  # Any character except newline
+    regex_pattern = regex_pattern.replace('{', '[^')  # [^abc] ->	not a, b, or c
     regex_pattern = regex_pattern.replace('}', ']')
-    regex_pattern = regex_pattern.replace('(', '{')     # a{5}	exactly five
+    regex_pattern = regex_pattern.replace('(', '{')  # a{5}	exactly five
     regex_pattern = regex_pattern.replace(')', '}')
-    regex_pattern = regex_pattern.replace('<', '}')     # ^abc$	start / end of the string
-    regex_pattern = regex_pattern.replace('>', '}')
+    regex_pattern = regex_pattern.replace('<', '^')  # ^abc$	start / end of the string
+    regex_pattern = regex_pattern.replace('>', '$')
 
     regex_pattern = regex_pattern.replace('?', '.')
-    regex_pattern = regex_pattern.replace('*', '.*?')   # '.*?' matches any number of any character, even zero (non-greedy -> shortest possible match)
-
+    regex_pattern = regex_pattern.replace('*', '.*?')  # '.*?' matches any number of any character, even zero (non-greedy -> shortest possible match)
     return regex_pattern
+
+
+
+def parse_fasta(fasta_file):
+    key = ''
+    sequence = ''
+
+    dict_sequences = {}
+
+    for line in fasta_file:
+        if line.startswith('>'):
+            key = line.strip('\n')
+            key = key.strip('>')
+            key = key.strip('\t')
+        else:
+            sequence += line.strip()
+
+
+    dict_sequences[key] = sequence
+
+    # print(key)
+
+    return dict_sequences
+
+
+def find_prosite_patterns(dict_sequences, pattern):
+    regex_pattern = re.compile(pattern)
+
+    # print(f'Pattern: {regex_pattern.pattern}')
+
+    matches = []
+
+    for key, value in dict_sequences.items():
+
+        # Go through sequence
+
+        for i in range(len(value)):
+            for j in range(i, len(value) + 1):
+
+                match = regex_pattern.fullmatch(value[i:j])
+
+                if match:
+                    matches.append({'key': key.strip(), 'start': i, 'sequence': match.group()})
+
+
+    return matches
 
 
 def main():
     args = parse_arguments()
 
+    # print(f'FASTA File: {args.fasta.name}')
+
     prosite_pattern = args.pattern
+    # print(f'Pattern: {prosite_pattern}')
 
-    print(f'Pattern: {prosite_pattern}')
-    print(f'Pattern as RegEx: {prosite_to_regex(prosite_pattern)}')
-    print(f'FASTA File: {args.fasta.name}')
+    pattern = prosite_to_regex(prosite_pattern)
+    # print(f'Pattern as RegEx: {regex_pattern}')
 
-    '''
-python psscan3.py --fasta misteriosa_virus.fasta --pattern "[LIVMF]-H-C-x(2)-G-x(3)-{STC}-[STAGP]-x-[LIVMFY]"
-Pattern: [LIVMF]-H-C-x(2)-G-x(3)-{STC}-[STAGP]-x-[LIVMFY]
-Pattern as RegEx: [LIVMF]HC.{2}G.{3}[^STC][STAGP].[LIVMFY]
-FASTA File: misteriosa_virus.fasta
-    '''
+
+    dict_sequences = parse_fasta(args.fasta)
+
+    matches = find_prosite_patterns(dict_sequences, pattern)
+
+    for match in matches:
+        print(f'{match["key"]}\t{match["start"]}\t{match["sequence"]}')
 
 
 if __name__ == "__main__":
