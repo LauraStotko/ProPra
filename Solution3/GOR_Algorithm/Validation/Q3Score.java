d73fb4898e286643fb9671c44eb02520e3b692c6
Laura Simone Louise Stotko <stotko@cip.ifi.lmu.de>
2024-03-12 17:37:55 +0100

    Replace Q3Score.java

--- Solution3/GOR_Algorithm/Validation/Q3Score.java
+++ Solution3/GOR_Algorithm/Validation/Q3Score.java
@@ -5,20 +5,32 @@ public class Q3Score {
 
     public static Map<Character, Double> calculateQ3Scores(String actualStructure, String predictedStructure) {
         Map<Character, Integer> correctPredictions = new HashMap<>();
-        Map<Character, Integer> totalPredictions = new HashMap<>();
+        Map<Character, Integer> totalObserved = new HashMap<>();
         correctPredictions.put('H', 0);
         correctPredictions.put('E', 0);
         correctPredictions.put('C', 0);
-        totalPredictions.put('H', 0);
-        totalPredictions.put('E', 0);
-        totalPredictions.put('C', 0);
+        totalObserved.put('H', 0);
+        totalObserved.put('E', 0);
+        totalObserved.put('C', 0);
+
+
+        int j = 0;
+        while (j< predictedStructure.length() && predictedStructure.charAt(j)=='-'){
+            j++;
+        }
+        if(j==predictedStructure.length())
+        {
+            return null;
+        }
+        actualStructure=actualStructure.substring(j, actualStructure.length()-j);
+        predictedStructure=predictedStructure.substring(j, predictedStructure.length()-j);
 
         for (int i = 0; i < actualStructure.length(); i++) {
             char actualChar = actualStructure.charAt(i);
             char predictedChar = predictedStructure.charAt(i);
 
-            if (totalPredictions.containsKey(actualChar)) {
-                totalPredictions.put(actualChar, totalPredictions.get(actualChar) + 1);
+            if (totalObserved.containsKey(actualChar)) {
+                totalObserved.put(actualChar, totalObserved.get(actualChar) + 1);
                 if (actualChar == predictedChar) {
                     correctPredictions.put(actualChar, correctPredictions.get(actualChar) + 1);
                 }
@@ -26,10 +38,10 @@ public class Q3Score {
         }
 
         Map<Character, Double> q3Scores = new HashMap<>();
-        for (Character structureType : totalPredictions.keySet()) {
-            double score = totalPredictions.get(structureType) > 0 ?
-                    (double) correctPredictions.get(structureType) / totalPredictions.get(structureType) * 100.0 :
-                    0.0;
+        for (Character structureType : totalObserved.keySet()) {
+            double score = totalObserved.get(structureType) > 0 ?
+                    (double) correctPredictions.get(structureType) / totalObserved.get(structureType) * 100.0 :
+                    -1.0;
             q3Scores.put(structureType, score);
         }
 
@@ -37,3 +49,4 @@ public class Q3Score {
     }
 
 }
+
